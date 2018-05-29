199465154106c351d95c723aa677f0f50443d54a
Olga Boiaryntseva <olgaboiar@gmail.com>
2018-05-29 16:17:54 -0400

    added soda gem

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,24 @@
 class WelcomeController < ApplicationController
+  #before_action :client
+  
   def index
+    if params[:building]
+      client = SODA::Client.new({:domain => "data.cityofnewyork.us", :app_token => ENV['NYC_OPEN_DATA_API_KEY']})
+      @passed = params[:building]
+      @response = client.get("b2iz-pps8", {"$limit" => 1, :boroid => 1})
+      puts "test"
+      puts @response
+      puts @response.first.registrationid
+    else
+
+    end
+    
   end
+
+
+  # private
+  #   def client
+  #     client = SODA::Client.new({:domain => "data.cityofnewyork.us", :app_token => ENV['NYC_OPEN_DATA_API_KEY']})
+  #   end
+  # end
 end
